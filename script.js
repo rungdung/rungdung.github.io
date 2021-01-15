901a01e61a5309f2df91c632fbd3a020b0c76d6d
Adhavan Sivaraj <71073145+rungdung@users.noreply.github.com>
2021-01-15 13:52:31 +0530

    Initial

---
+++ script.js
@@ -0,0 +1,18 @@
+let canvas;
+let myMap;
+
+const mappa=new Mappa('Leaflet');
+
+const options = {
+    lat: 40.73447,
+    lng: -74.00232,
+    zoom: 13,
+    style: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
+  }
+
+
+function setup(){
+    canvas = createCanvas(800,700);
+    myMap = mappa.tileMap(options); 
+    myMap.overlay(canvas); // Overlay the canvas on top of the map.
+  }
